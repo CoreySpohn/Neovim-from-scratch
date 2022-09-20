df18b3735b50a2e7635cec6c3d7adc2b8f7fd03c
Corey Spohn <cas584@cornell.edu>
2022-09-20 16:21:12 -0400

    Initial update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,7 +61,7 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
-	use("lunarvim/darkplus.nvim")
+  use({"flazz/vim-colorschemes"})
 
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
